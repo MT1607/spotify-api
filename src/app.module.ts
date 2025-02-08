b2bfd5912ba1d32127db8ad2ce46b70564bffbfc
MT1607 <nguyengiason5@gmail.com>
2025-02-08 14:39:01 +0700

    create-module-palylist

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 
 import configuration from './config/configuration';
 import { HttpModule } from '@nestjs/axios';
+import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     HttpModule,
+    PlaylistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/playlist/playlist.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PlaylistService } from './playlist.service';
+import { PlaylistController } from './playlist.controller';
+
+@Module({
+  providers: [PlaylistService],
+  controllers: [PlaylistController],
+})
+export class PlaylistModule {
+  constructor(private readonly playlistService: PlaylistService) {}
+}
